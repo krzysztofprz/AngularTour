287e9fd600fa93b4ab2f3e627fd966d65ab3b1ad
krzysztofprz <34395591+krzysztofprz@users.noreply.github.com>
2019-11-22 21:49:42 +0100

    first page with form

--- math-tour/src/app/app-routing.module.ts
+++ math-tour/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
+import { DateFactsComponent } from './../content/date-facts/date-facts.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/DateFact', pathMatch: 'full' },
+  { path: 'DateFact', component: DateFactsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- math-tour/src/app/app.module.ts
+++ math-tour/src/app/app.module.ts
@@ -1,21 +1,31 @@
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { DateFactsComponent } from './../content/date-facts/date-facts.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DateFactsComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    NgbModule,
+    ReactiveFormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule {
-  title = 'Math Tour'
+  title = 'Math Tour';
 
 }
